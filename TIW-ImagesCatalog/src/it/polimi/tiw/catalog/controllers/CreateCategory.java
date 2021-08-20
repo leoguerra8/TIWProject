da490c63fbb4a31448e0e60e09453e95b160d38e
Gaia Locchi <gaialocchi@outlook.it>
2021-08-20 20:03:19 +0200

    Create Category

--- TIW-ImagesCatalog/src/it/polimi/tiw/catalog/controllers/CreateCategory.java
+++ TIW-ImagesCatalog/src/it/polimi/tiw/catalog/controllers/CreateCategory.java
@@ -2,6 +2,7 @@ package it.polimi.tiw.catalog.controllers;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -18,24 +19,27 @@ import it.polimi.tiw.catalog.utils.ConnectionHandler;
 @WebServlet("/CreateCategory")
 @MultipartConfig
 public class CreateCategory extends HttpServlet {
+	private static final int MAX_CATEGORIES = 9;
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Connection connection = null;
-	
+
 	public CreateCategory() {
 		super();
 	}
-	
+
 	public void init() throws ServletException {
 		connection = ConnectionHandler.getConnection(getServletContext());
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		boolean isBadRequest = false;
 		String name = null;
 		Integer fatherId = null;
-		
+		String code = null;
+
 		try {
 			name = StringEscapeUtils.escapeJava(request.getParameter("name"));
 			fatherId = Integer.parseInt(request.getParameter("fatherId"));
@@ -43,16 +47,68 @@ public class CreateCategory extends HttpServlet {
 			isBadRequest = true;
 			e.printStackTrace();
 		}
-		
+
 		if (isBadRequest) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			response.getWriter().println("Incorrect or missing request parameters");
 			return;
 		}
-		
+
 		// Create Category
 		int categoryId;
 		CategoryDAO categoryDAO = new CategoryDAO(connection);
-		// TODO
+
+		if(fatherId == null) { // The category is a root
+			try {
+			int rootsNo = categoryDAO.getNumOfRoots();
+			if(rootsNo == MAX_CATEGORIES) {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				response.getWriter().println("The number of root categories cannot be more that 9");
+				return;
+			} else { 
+				code = String.valueOf(rootsNo+1);
+			} 
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} else { // The category is a sub-category
+			String lastChildCode;
+			try {
+				lastChildCode = categoryDAO.findLastChildCode(fatherId);
+
+				int lastDigit = Integer.valueOf(lastChildCode.charAt(lastChildCode.length()-1));
+				if(lastDigit == MAX_CATEGORIES) {
+					response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+					response.getWriter().println("The number of sub-categories cannot be more that 9");
+					return;
+				} else {
+					code = lastChildCode.substring(0, lastChildCode.length()-1) + String.valueOf(lastDigit+1);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			categoryId = categoryDAO.createCategory(name, code, fatherId);
+		} catch (SQLException e) {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			response.getWriter().println("Not possible to create category");
+			return;
+		}
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().print(categoryId);
+
 	}
+
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
--- TIW-ImagesCatalog/src/it/polimi/tiw/catalog/dao/CategoryDAO.java
+++ TIW-ImagesCatalog/src/it/polimi/tiw/catalog/dao/CategoryDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,20 +13,20 @@ import it.polimi.tiw.catalog.beans.Category;
 
 public class CategoryDAO {
 	private Connection connection;
-	
+
 	public CategoryDAO(Connection c) {
 		this.connection = c;
 	}
-	
+
 	public int createCategory(String name, String code, int fatherId) throws SQLException {
-		
+
 		String query = "INSERT into users (name, code, father) VALUES (?, ?, ?)";
 		try (PreparedStatement pStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);) {
 			pStatement.setString(1, name);
 			pStatement.setString(2, code);
 			pStatement.setInt(3, fatherId);
 			pStatement.executeUpdate();
-			
+
 			ResultSet generatedKeys = pStatement.getGeneratedKeys();
 			if (generatedKeys.next()) {
 				return generatedKeys.getInt(1);
@@ -34,26 +35,26 @@ public class CategoryDAO {
 			}
 		}
 	}
-	
+
 	public String findLastChildCode(int categoryId) throws SQLException {
 		String code = null;
-		
+
 		String query = "SELECT MAX(code) FROM categories WHERE father = ?";
-		
+
 		try (PreparedStatement pStatement = connection.prepareStatement(query);) {
 			pStatement.setInt(1, categoryId);
 			ResultSet res = pStatement.executeQuery();
-				if (res.next()) {
-					code = res.getString("code");
-				}
+			if (res.next()) {
+				code = res.getString("code");
+			}
 		}
-		
+
 		return code;
 	}
-	
+
 	public List<Category> findCategoriesByFather(int fatherId) throws SQLException {
 		List<Category> categories = new ArrayList<Category>();
-		
+
 		String query = "SELECT * FROM categories WHERE father = ? ORDER BY code";
 		try (PreparedStatement pStatement = connection.prepareStatement(query);) {
 			pStatement.setInt(1, fatherId);
@@ -69,10 +70,10 @@ public class CategoryDAO {
 		}
 		return categories;
 	}
-	
+
 	public List<Category> findAllCategories() throws SQLException {
 		List<Category> categories = new ArrayList<Category>();
-		
+
 		String query = "SELECT * FROM categories ORDER BY code";
 		try (PreparedStatement pStatement = connection.prepareStatement(query);) {
 			try (ResultSet res = pStatement.executeQuery();) {
@@ -87,16 +88,16 @@ public class CategoryDAO {
 		}
 		return categories;
 	}
-	
+
 	public int updateCategory(int categoryId, String code, int fatherId) throws SQLException {
 		String query = "UPDATE categories SET code = ?, fatherId = ? WHERE categoryId = ? ";
 		try (PreparedStatement pStatement = connection.prepareStatement(query);) {
 			pStatement.setString(1, code);
 			pStatement.setInt(2,  fatherId);
 			pStatement.setInt(3, categoryId);
-			
+
 			pStatement.executeUpdate();
-		
+
 			ResultSet generatedKeys = pStatement.getGeneratedKeys();
 			if (generatedKeys.next()) {
 				return generatedKeys.getInt(1);
@@ -105,5 +106,20 @@ public class CategoryDAO {
 			}
 		}
 	}
-	
-}
+
+	public int getNumOfRoots() throws SQLException {
+		String query = "SELECT count(*) AS rootsNo FROM categories WHERE fatherId = NULL";
+		try(Statement statement = connection.createStatement();) {	
+			ResultSet result = statement.executeQuery(query);
+			if(result.next()) {
+				return result.getInt("rootsNo");
+			} else {
+				throw new SQLException("Something went wrong while retrieving the number of root categories");
+			}
+		} catch (SQLException | NumberFormatException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+
+	}
