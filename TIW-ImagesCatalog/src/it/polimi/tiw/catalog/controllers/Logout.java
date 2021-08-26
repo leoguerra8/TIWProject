67332e8c950e7a94d576cebc961162fad6ccaa61
Gaia Locchi <gaialocchi@outlook.it>
2021-08-26 12:33:14 +0200

    Logout and restyling

--- TIW-ImagesCatalog/src/it/polimi/tiw/catalog/controllers/Logout.java
+++ TIW-ImagesCatalog/src/it/polimi/tiw/catalog/controllers/Logout.java
@@ -7,31 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Logout
- */
-
-/*
- * Questa servlet si occupa delle richieste di GET da parte di un utente, che è attualmente
- * loggato, e clicca su "Logout", invalidando la sessione
- */
-
 @WebServlet("/Logout")
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public Logout() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
