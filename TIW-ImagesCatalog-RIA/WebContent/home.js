d81a2a4965ac92b7ff830c1962f5d3cd010caeb4
Gaia Locchi <gaialocchi@outlook.it>
2021-09-02 22:11:35 +0200

    Drag&drop fe update

--- TIW-ImagesCatalog-RIA/WebContent/home.js
+++ TIW-ImagesCatalog-RIA/WebContent/home.js
@@ -1,5 +1,9 @@
 (function() {
 	let categoriesList, categoryForm, pageOrchestrator = new PageOrchestrator(); // main controller
+	let startElement;
+	let updateQueue = [];
+	let allCategories = [];
+	const MAX_CATEGORIES = 9;
 
 	window.addEventListener("load", () => {
 		if (sessionStorage.getItem("username") == null) {
@@ -10,9 +14,97 @@
 		}
 	}, false);
 
-	const toText = (category) => {
-		const spacing = ">".repeat(category.code.length - 1);
+	function toText(category) {
+		var spacing = ">".repeat(category.code.length - 1);
 		return spacing + " " + category.code + " " + category.name;
+	}
+
+	function belongsToSubtree(catCode, fatCode) {
+		return fatCode.substring(0, catCode.length) == catCode;
+	}
+
+	function dragStartHandler(event) {
+		startElement = event.target.closest("tr");
+	}
+
+	function dragOverHandler(event) {
+		event.preventDefault();
+		var dest = event.target.closest("tr");
+		dest.className = "selected";
+	}
+
+	function dragLeaveHandler(event) {
+		var dest = event.target.closest("tr");
+		dest.className = "not-selected";
+	}
+
+	function dropHandler(event) {
+		var dest = event.target.closest("tr");
+		var catId = startElement.getAttribute("categoryId");
+		var oldCatCode = startElement.getAttribute("categoryCode");
+		var oldFatId = startElement.getAttribute("fatherId");
+		var newFatId = dest.getAttribute("categoryId");
+		var newFatCode = dest.getAttribute("categoryCode");
+		var isAllowed = true;
+		if (newFatId == oldFatId || belongsToSubtree(oldCatCode, newFatCode) || newFatId == catId) { isAllowed = false; }
+		if (isAllowed) {
+		updateQueue.push({
+			categoryId: catId,
+			oldCategoryCode: oldCatCode,
+			oldFatherId: oldFatId,
+			newFatherId: newFatId,
+			newFatherCode: newFatCode
+		});
+		moveAndUpdate(parseInt(catId), oldCatCode, newFatCode, parseInt(oldFatId), parseInt(newFatId));
+		categoriesList.update(allCategories.sort(function(c1, c2) { return (c1.code).localeCompare(c2.code) }));
+		} else {
+			dest.className = "not-selected";
+		}
+	}
+
+	function moveAndUpdate(catId, oldCatCode, newFatCode, oldFatId, newFatId) {
+		var lastChildNewFatherCode = Math.max.apply(Math, allCategories.filter(c => c.fatherId == newFatId).map(function(c) { return c.code; }));
+		if(!isFinite(lastChildNewFatherCode)) {
+			lastChildNewFatherCode = "-1";
+		}
+		var lastChildOldFatherCode = "-1";
+		var newCatCode;
+		if (oldFatId) {
+			lastChildOldFatherCode = Math.max.apply(Math, allCategories.filter(c => c.fatherId == oldFatId).map(function(c) { return c.code; }));
+		}
+		lastChildNewFatherCode = lastChildNewFatherCode.toString();
+		var lastDigit = parseInt(lastChildNewFatherCode.charAt(lastChildNewFatherCode.length-1));
+		// calculate new category code
+		if (lastDigit == MAX_CATEGORIES) {
+			window.alert("The number of sub-categories cannot be more than 9");
+			this.pageOrchestrator.refresh();
+		} else {
+			if (lastChildOldFatherCode == newFatCode) {
+				if (lastChildNewFatherCode == -1) { newCatCode = oldCatCode + "1"; }
+				else newCatCode = oldCatCode + (lastDigit+1).toString();
+			}
+			if (lastChildNewFatherCode == "-1") { newCatCode = newFatCode + "1"; }
+			else newCatCode = newFatCode + (lastDigit+1).toString();
+		}
+
+		// update categories
+		var i = allCategories.findIndex(c => c.id == catId);
+		allCategories[i].code = newCatCode;
+		allCategories[i].fatherId = newFatId;
+		var catChildren = allCategories.filter(function(c) { return c.code.substring(0, oldCatCode.length) == oldCatCode});
+		catChildren.forEach(function(child) {
+			child.code = newCatCode + child.code.substring(oldCatCode.length);
+		});
+		var lastBroCode = Math.max.apply(Math, allCategories.filter(c => c.fatherId == oldFatId).map(function(c) { return c.code; }));
+		if (isFinite(lastBroCode) && lastBroCode.toString().localeCompare(oldCatCode) == 1) {
+			lastBroCode = lastBroCode.toString();
+			var j = allCategories.findIndex(c => c.code == lastBroCode);
+			allCategories[j].code = oldCatCode;
+			broChildren = allCategories.filter(function(c) { return c.code.substring(0, lastBroCode.length) == lastBroCode });
+			broChildren.forEach(function(broChild) {
+				broChild.code = oldCatCode + broChild.code.substring(lastBroCode.length);
+			})
+		}
 	}
 
 	// Constructors of view components
@@ -39,8 +131,9 @@
 				function(req) {
 					if (req.readyState == 4) {
 						var message = req.responseText;
-						if (req.status == 200) {
+						if (req.status == 200) { 
 							var categories = JSON.parse(req.responseText);
+							allCategories = [...categories];
 							if (categories.length == 0) {
 								self.alert.textContent = "No categories yet!";
 								return;
@@ -57,28 +150,26 @@
 		}
 
 		this.update = function(arrayCategories) {
-			var row, linkcell, linkText, anchor;
+			var row, cell;
 			this.listcontainerbody.innerHTML = ""; // empty the table body
 			var self = this;
 			arrayCategories.forEach(function(category) {
 				row = document.createElement("tr");
 				row.setAttribute("draggable", "true");
-				row.setAttribute("ondragstart", "dragStartHandler(event)");
-                row.setAttribute("ondrop", "dropOverHandler(event)");
-                row.setAttribute("ondragover", "allowDrop(event)");
-                row.setAttribute("ondragenter", "dragEnter(event)");
-                row.setAttribute("ondragleave", "dragLeave(event)");
-                row.setAttribute("class", "spaceUnder");
+				row.addEventListener("dragstart", dragStartHandler);
+                row.addEventListener("dragover", dragOverHandler);
+                row.addEventListener("dragleave", dragLeaveHandler);
+				row.addEventListener("drop", dropHandler);
                 row.setAttribute("id", category.id);
                 row.setAttribute("code", category.code);
-                linkcell = document.createElement("td");
+                cell = document.createElement("td");
                 anchor = document.createElement("a");
-                linkcell.appendChild(anchor);
-                linkText = document.createTextNode(toText(category));
-                anchor.appendChild(linkText);
-                anchor.setAttribute('cateogoryid', category.id);
-                anchor.href = "#";
-                row.appendChild(linkcell);
+                cell.appendChild(anchor);
+                cell.textContent = toText(category);
+                row.setAttribute('categoryId', category.id);
+                row.setAttribute('categoryCode', category.code);
+				row.setAttribute('fatherId', category.fatherId);
+                row.appendChild(cell);
                 self.listcontainerbody.appendChild(row);
 			});
 			this.listcontainer.style.visibility = "visible";	
